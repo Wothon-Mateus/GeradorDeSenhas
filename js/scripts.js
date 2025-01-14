f32dda9906273ff096c72a475afca8580a88be48
Wothon-Mateus <13wothon@gmail.com>
2025-01-13 21:12:10 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Seleção de elementos
+// Seleção  de elementos
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 
